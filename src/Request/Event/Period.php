29df959e69750ed2b69bef5502b0b528a0517e47
Daniel Kurowski <daniel.kurowski@grifart.cz>
2022-01-11 11:29:56 +0100

    replace excludeRunning() with full featured Period filter

---
+++ src/Request/Event/Period.php
@@ -0,0 +1,27 @@
+<?php declare(strict_types = 1);
+
+namespace HnutiBrontosaurus\BisClient\Request\Event;
+
+use Grifart\Enum\AutoInstances;
+use Grifart\Enum\Enum;
+
+
+/**
+ * @method static Period UNLIMITED()
+ * @method static Period RUNNING_ONLY()
+ * @method static Period FUTURE_ONLY()
+ * @method static Period PAST_ONLY()
+ * @method static Period RUNNING_AND_FUTURE()
+ * @method static Period RUNNING_AND_PAST()
+ */
+final class Period extends Enum
+{
+	use AutoInstances;
+
+	protected const UNLIMITED = 'unlimited';
+	protected const RUNNING_ONLY = 'runningOnly';
+	protected const FUTURE_ONLY = 'futureOnly';
+	protected const PAST_ONLY = 'pastOnly';
+	protected const RUNNING_AND_FUTURE = 'runningAndFuture';
+	protected const RUNNING_AND_PAST = 'runningAndPast';
+}
