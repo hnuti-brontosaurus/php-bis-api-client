ae4c4413c240ae86313c1885eba389f05940b276
Daniel Kurowski <daniel.kurowski@grifart.cz>
2023-01-03 11:21:05 +0100

    add opportunities tests to index

--- tests/index.php
+++ tests/index.php
@@ -2,4 +2,6 @@
 	<li><a href="events.php">events</a></li>
 	<li><a href="event.php">event</a></li>
 	<li><a href="organizational-units.php">organizational units</a></li>
+	<li><a href="opportunities.php">opportunities</a></li>
+	<li><a href="opportunity.php">opportunity</a></li>
 </ul>
