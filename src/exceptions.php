74ffb6b260c5ce178ea752dfadb8b70792ef70e2
Daniel Kurowski <daniel.kurowski@grifart.cz>
2023-01-09 16:21:58 +0100

    BisClientRuntimeException -> RuntimeException

--- src/exceptions.php
+++ src/exceptions.php
@@ -6,23 +6,19 @@ namespace HnutiBrontosaurus\BisClient;
 /**
  * Exception caused by bad usage by developer (preventable)
  * e.g. called method in a wrong moment
- * @internal
+ * Generic catch-all exception for all subtypes.
  */
 class UsageException extends \LogicException {}
 
 /**
  * Exception caused by unpredictable circumstances on server (not preventable)
  * e.g. file does not exist, external system not accessible etc.
- * @internal
+ * Generic catch-all exception for all subtypes.
  */
 class RuntimeException extends \RuntimeException {}
 
 
-// common catch-all exception
-class BisClientRuntimeException extends RuntimeException {}
-
-
-final class UnableToProcessRequest extends BisClientRuntimeException
+final class UnableToProcessRequest extends RuntimeException
 {
 	public static function withPrevious(\Throwable $previous): self
 	{
@@ -30,7 +26,7 @@ final class UnableToProcessRequest extends BisClientRuntimeException
 	}
 }
 
-final class UnableToAuthorize extends BisClientRuntimeException
+final class UnableToAuthorize extends RuntimeException
 {
 	public static function withPrevious(\Throwable $previous): self
 	{
@@ -38,14 +34,14 @@ final class UnableToAuthorize extends BisClientRuntimeException
 	}
 }
 
-final class NotFound extends BisClientRuntimeException
+final class NotFound extends RuntimeException
 {
 	public static function withPrevious(\Throwable $previous): self
 	{
 		return new self('The target you requested was not found. Check again that you\'ve typed correct URL or that the resource exists.', 0, $previous);
 	}
 }
-final class ConnectionToBisFailed extends BisClientRuntimeException
+final class ConnectionToBisFailed extends RuntimeException
 {
 	public static function withPrevious(\Throwable $previous): self
 	{
