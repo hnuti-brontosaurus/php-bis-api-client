8ee2c016f92fdfc842543b7be272a60b4b157187
dakur <dev@kurowsti.cz>
2026-04-16 20:01:12 +0000

    fix tests

--- tests/events.php
+++ tests/events.php
@@ -16,37 +16,37 @@ echo '<h2>Events</h2>';
 
 $params = new EventParameters();
 $params->setGroups([
-	Group::CAMP(),
-	Group::WEEKEND_EVENT(),
-	Group::OTHER(),
+	Group::CAMP,
+	Group::WEEKEND_EVENT,
+	Group::OTHER,
 ]);
 $params->setCategories([
-	Category::EVP(),
-	Category::EXPERIENTAL(),
-	Category::INTERNAL(),
-	Category::INTERNAL_EDUCATIONAL(),
-	Category::PRESENTATION(),
-	Category::PUBLIC_EDUCATIONAL(),
-	Category::SECTION_EVENT(),
-	Category::SECTION_MEETING(),
-	Category::VOLUNTEERING(),
+	Category::EVP,
+	Category::EXPERIENTAL,
+	Category::INTERNAL,
+	Category::INTERNAL_EDUCATIONAL,
+	Category::PRESENTATION,
+	Category::PUBLIC_EDUCATIONAL,
+	Category::SECTION_EVENT,
+	Category::SECTION_MEETING,
+	Category::VOLUNTEERING,
 ]);
 $params->setPrograms([
-	Program::NONE(),
-	Program::NATURE(),
-	Program::MONUMENTS(),
-	Program::KIDS(),
-	Program::ECO_TENT(),
-	Program::HOLIDAYS_WITH_BRONTOSAURUS(),
-	Program::EDUCATION(),
-	Program::INTERNATIONAL(),
+	Program::NONE,
+	Program::NATURE,
+	Program::MONUMENTS,
+	Program::KIDS,
+	Program::ECO_TENT,
+	Program::HOLIDAYS_WITH_BRONTOSAURUS,
+	Program::EDUCATION,
+	Program::INTERNATIONAL,
 ]);
 $params->setMultipleIntendedFor([
-	IntendedFor::ALL(),
-	IntendedFor::YOUNG_AND_ADULT(),
-	IntendedFor::KIDS(),
-	IntendedFor::PARENTS_WITH_KIDS(),
-	IntendedFor::FIRST_TIME_PARTICIPANT(),
+	IntendedFor::ALL,
+	IntendedFor::YOUNG_AND_ADULT,
+	IntendedFor::KIDS,
+	IntendedFor::PARENTS_WITH_KIDS,
+	IntendedFor::FIRST_TIME_PARTICIPANT,
 ]);
 $events = $client->getEvents($params);
 foreach ($events as $event) {
